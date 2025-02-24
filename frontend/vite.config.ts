b06b20acd765cf5151d2c36930e3411f53f7aa33
Alex <mihalex.cris@yahoo.com>
2025-02-24 03:28:25 +0200

    updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Add this line
+  base: '/',  // Add this line
   build: {
     outDir: 'dist',  // Specify output directory
     rollupOptions: {
