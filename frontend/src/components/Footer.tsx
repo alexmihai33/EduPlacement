bc76b9dcfe0dee0c7443d5005eae324c58078cd9
Alex <mihalex.cris@yahoo.com>
2025-02-25 14:26:06 +0200

    Date function for Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,22 @@
-const Footer = () => (
+const Footer = () => {
+
+  function getYear(){
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return year;
+  }
+
+  const currentYear = getYear()
+  
+  return(
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
     <p>
-      Ministerul Educatiei 2025
+      Ministerul Educatiei {currentYear}
     </p>
   </footer>
-);
+  );
+};
 
 export default Footer;
